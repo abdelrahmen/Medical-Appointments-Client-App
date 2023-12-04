8b95fbae13031c63f8e6bf5fc716ff785fa4e9f5
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-12-04 16:13:58 +0200

    routing and other essential changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { AppointmentListComponent } from './Components/appointment-list/appointment-list.component';
+import { SchedualAppointmentComponent } from './Components/schedual-appointment/schedual-appointment.component';
+import { BookedAppointmentsComponent } from './Components/booked-appointments/booked-appointments.component';
+import { MedicalHistoryComponent } from './Components/medical-history/medical-history.component';
+import { EditMedicalHistoryComponent } from './Components/edit-medical-history/edit-medical-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "home", component: AppointmentListComponent},
+  {path: "schedual/:appointmentId", component: SchedualAppointmentComponent},
+  {path: "booked", component: BookedAppointmentsComponent},
+  {path: "medical-history", component: MedicalHistoryComponent},
+  {path: "medical-history/:medicalHistoryId", component: EditMedicalHistoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {ProgressSpinnerMode, MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { HeaderComponent } from './Components/header/header.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './Components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppointmentListComponent } from './Components/appointment-list/appointment-list.component';
+import { SchedualAppointmentComponent } from './Components/schedual-appointment/schedual-appointment.component';
+import { BookedAppointmentsComponent } from './Components/booked-appointments/booked-appointments.component';
+import { MedicalHistoryComponent } from './Components/medical-history/medical-history.component';
+import { EditMedicalHistoryComponent } from './Components/edit-medical-history/edit-medical-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
+    AppointmentListComponent,
+    SchedualAppointmentComponent,
+    BookedAppointmentsComponent,
+    MedicalHistoryComponent,
+    EditMedicalHistoryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
