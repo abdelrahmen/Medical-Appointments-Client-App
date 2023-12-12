998c88d7bacd0b35f5aef5e8cd498b089ea13e69
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-12-12 21:37:53 +0200

    enhanced Logic

--- src/app/Components/booked-appointments/booked-appointments.component.ts
+++ src/app/Components/booked-appointments/booked-appointments.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AppointmentDTO } from 'src/app/Models/AppointmentDTO';
 import { AppointmentService } from 'src/app/Services/appointment.service';
@@ -10,17 +11,24 @@ import { AppointmentService } from 'src/app/Services/appointment.service';
 export class BookedAppointmentsComponent implements OnInit {
 
   scheduledAppointments: AppointmentDTO[] = [];
+  isLoading: boolean = true;
 
   constructor(private appointmentService: AppointmentService) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
     this.appointmentService.getSchedualledAppointments().subscribe({
       next: (appointments) => {
         this.scheduledAppointments = appointments;
+        this.isLoading = false;
       },
-      error: (error) => {
-        console.error('Error fetching scheduled appointments:', error);
+      error: (err) => {
+        this.openSnackBar(err.message);
+        this.isLoading = false;
       },
+      complete: ()=>{
+        this.isLoading = false;
+      }
     }
     );
   }
@@ -36,19 +44,29 @@ export class BookedAppointmentsComponent implements OnInit {
   };
 
   cancelAppointmnet(appointmentId: number) {
+    this.isLoading = true;
     this.appointmentService.cancelAppointment(appointmentId).subscribe({
       next: (res)=>{
         console.log(res);
         this.ngOnInit();
+        this.isLoading = false;
+      },
+      error: (err: HttpErrorResponse)=>{
+        this.openSnackBar(err.message);
+        this.isLoading = false;
+      },
+      complete: ()=>{
+        this.isLoading = false;
       }
     });
   }
 
-  isLoading: boolean = true;
+  
 
   showSnackBar: boolean=false;
   snackbarMessage:string="";
   openSnackBar(msg:string) {
+    console.log("in open snack bar");
     this.showSnackBar = true;
     this.snackbarMessage=msg;
     setTimeout(()=>this.showSnackBar=false, 3000);
