fd793b0d4f4dd8d77b8905c080dedcb4fb89108e
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-12-12 21:33:21 +0200

    made options in a function to be updated upon request

--- src/app/Services/medical-history.service.ts
+++ src/app/Services/medical-history.service.ts
@@ -16,19 +16,23 @@ export class MedicalHistoryService {
   constructor(private httpClient: HttpClient) {
   }
 
-  options = {
-    headers: new HttpHeaders({
-      "Authorization": `Bearer ${localStorage.getItem("token")}`,
-      // "Content-Type": "application/json",
-      //"Cache-Control": "max-age=3600",
-      // "Access-Control-Allow-Origin": "http://localhost:4200",
-      //"ETag": "123456",
-      //"Location": "https://example.com/new-resource",
+  getOptions() {
+
+    let options = {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        // "Content-Type": "application/json",
+        //"Cache-Control": "max-age=3600",
+        // "Access-Control-Allow-Origin": "http://localhost:4200",
+        //"ETag": "123456",
+        //"Location": "https://example.com/new-resource",
       //"X-Content-Type-Options": "nosniff",
       //"X-Frame-Options": "DENY",
-      //"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-      //"X-Powered-By": "ASP.NET Core"
-    })
+        //"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+        //"X-Powered-By": "ASP.NET Core"
+      })
+    }
+    return options;
   }
 
   handleErrors(err: HttpErrorResponse) {
@@ -46,42 +50,42 @@ export class MedicalHistoryService {
 
 
   getAll() {
-    return this.httpClient.get<MedicalHistoryDTO[]>(`${environment.baseUrl}/api/medicalhistory`, this.options).pipe(
-      catchError((err, caught) => {
-        this.handleErrors(err);
-        return of();
-      }),
+    return this.httpClient.get<MedicalHistoryDTO[]>(`${environment.baseUrl}/api/medicalhistory`, this.getOptions()).pipe(
+      // catchError((err, caught) => {
+      //   this.handleErrors(err);
+      //   return of();
+      // }),
       tap(data => this.medicalHistories = data),
     )
   }
 
   addMedicalHistory(medicalHistoryDTO: CreateMedicalHistoryDTO) {
-    return this.httpClient.post(`${environment.baseUrl}/api/medicalhistory`, medicalHistoryDTO, this.options).pipe(
-      catchError((err, caught) => {
-        console.log(err);
-        this.handleErrors(err);
-        return of();
-      })
+    return this.httpClient.post(`${environment.baseUrl}/api/medicalhistory`, medicalHistoryDTO, this.getOptions()).pipe(
+      // catchError((err, caught) => {
+      //   console.log(err);
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     );
   }
 
   deleteRecord(recordId: number) {
-    return this.httpClient.delete(`${environment.baseUrl}/api/medicalhistory/${recordId}`, this.options).pipe(
-      catchError((err, caught) => {
-        console.log(err);
-        this.handleErrors(err);
-        return of();
-      })
+    return this.httpClient.delete(`${environment.baseUrl}/api/medicalhistory/${recordId}`, this.getOptions()).pipe(
+      // catchError((err, caught) => {
+      //   console.log(err);
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     );
   }
 
   updateRecord(medicalHistory: UpdateMedicalHistoryDTO){
-    return this.httpClient.put(`${environment.baseUrl}/api/medicalhistory/${medicalHistory.medicalHistoryID}`, medicalHistory, this.options).pipe(
-      catchError((err, caught) => {
-        console.log(err);
-        this.handleErrors(err);
-        return of();
-      })
+    return this.httpClient.put(`${environment.baseUrl}/api/medicalhistory/${medicalHistory.medicalHistoryID}`, medicalHistory, this.getOptions()).pipe(
+      // catchError((err, caught) => {
+      //   console.log(err);
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     )
   }
 }
