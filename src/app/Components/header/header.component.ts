db7fa763025a63db8af8bd043619078040b98b1a
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-12-12 21:38:46 +0200

    added missing links and improved Logic

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  Logout() {
+    this.authService.logout().subscribe({
+      next: (res) => {
+        this.openSnackBar("logged out succefully")
+      },
+      error: (err: HttpErrorResponse) => {
+        this.openSnackBar(err.message);
+      }
+    });
+  }
+
+  showSnackBar: boolean = false;
+  snackbarMessage: string = "";
+  openSnackBar(msg: string) {
+    this.showSnackBar = true;
+    this.snackbarMessage = msg;
+    setTimeout(() => this.showSnackBar = false, 3000);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
