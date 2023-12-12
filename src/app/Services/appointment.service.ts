99ea3038605ea24b4d7874f2362d93db0963e291
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-12-12 21:35:01 +0200

    made options in a function to be updated upon request

--- src/app/Services/appointment.service.ts
+++ src/app/Services/appointment.service.ts
@@ -8,14 +8,14 @@ import { catchError, of, retry } from 'rxjs';
   providedIn: 'root'
 })
 export class AppointmentService {
-  
-  constructor(private httpClient: HttpClient) {}
 
-  handleErrors(err:HttpErrorResponse){
+  constructor(private httpClient: HttpClient) { }
+
+  handleErrors(err: HttpErrorResponse) {
     switch (err.status) {
       case 400:
         console.log(err);
-          //alert(`${err?.error[0]["description"]}`)
+        //alert(`${err?.error[0]["description"]}`)
         break;
       case 500:
         alert("internal server error");
@@ -26,55 +26,59 @@ export class AppointmentService {
     console.log(err)
   }
 
-  options = {
-    headers: new HttpHeaders({
-      "Authorization": `Bearer ${localStorage.getItem("token")}`,
-      //"Content-Type": "application/json",
-      //"Cache-Control": "max-age=3600",
-      //"Access-Control-Allow-Origin": "http://localhost:4200",
-      //"ETag": "123456",
-      //"Location": "https://example.com/new-resource",
-      //"X-Content-Type-Options": "nosniff",
-      //"X-Frame-Options": "DENY",
-      //"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-      //"X-Powered-By": "ASP.NET Core"
-    })
+
+  getOptions() {
+    let options = {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        //"Content-Type": "application/json",
+        //"Cache-Control": "max-age=3600",
+        //"Access-Control-Allow-Origin": "http://localhost:4200",
+        //"ETag": "123456",
+        //"Location": "https://example.com/new-resource",
+        //"X-Content-Type-Options": "nosniff",
+        //"X-Frame-Options": "DENY",
+        //"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+        //"X-Powered-By": "ASP.NET Core"
+      })
+    }
+    return options;
   }
 
-  getAvailableAppointments(){
+  getAvailableAppointments() {
     return this.httpClient.get<AppointmentDTO[]>(`${environment.baseUrl}/api/appointments/available`).pipe(
       retry(2),
-      catchError((err, caught)=>{
-        this.handleErrors(err);
-        return of();
-      })
+      // catchError((err, caught)=>{
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     );
   }
 
-  bookAppointment(appointmentDTO?:AppointmentDTO){
-    return this.httpClient.put(`${environment.baseUrl}/api/appointments/book/${appointmentDTO?.appointmentID}`,appointmentDTO, this.options).pipe(
-      catchError((err)=>{
-        this.handleErrors(err);
-        return of();
-      })
+  bookAppointment(appointmentDTO?: AppointmentDTO) {
+    return this.httpClient.put(`${environment.baseUrl}/api/appointments/book/${appointmentDTO?.appointmentID}`, appointmentDTO, this.getOptions()).pipe(
+      // catchError((err)=>{
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     )
   }
 
-  getSchedualledAppointments(){
-    return this.httpClient.get<AppointmentDTO[]>(`${environment.baseUrl}/api/appointments/my-appointments`,this.options).pipe(
-      catchError((err)=>{
-        this.handleErrors(err);
-        return of();
-      })
+  getSchedualledAppointments() {
+    return this.httpClient.get<AppointmentDTO[]>(`${environment.baseUrl}/api/appointments/my-appointments`, this.getOptions()).pipe(
+      // catchError((err)=>{
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     )
   }
 
   cancelAppointment(appointmentId: number) {
-    return this.httpClient.put(`${environment.baseUrl}/api/appointments/${appointmentId}`,{},this.options).pipe(
-      catchError((err)=>{
-        this.handleErrors(err);
-        return of();
-      })
+    return this.httpClient.put(`${environment.baseUrl}/api/appointments/${appointmentId}`, {}, this.getOptions()).pipe(
+      // catchError((err)=>{
+      //   this.handleErrors(err);
+      //   return of();
+      // })
     )
   }
 
